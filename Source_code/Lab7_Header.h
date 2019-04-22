dbe20314c5b2de7c295983c99cd0dceb4097e950
Ali <alizim110@gmail.com>
2019-04-22 15:35:38 -0500

    error fix

--- Source_code/Lab7_Header.h
+++ Source_code/Lab7_Header.h
@@ -1,10 +1,10 @@
-#ifndef GPIO_DEFS_H
+/*#ifndef GPIO_DEFS_H
 #define GPIO_DEFS_H
 
 //Define a macro for left shifting operation;
 // UL: Unsigned Long (data type)
 
-#define MASK(x) (1UL << (x))
+
 
 // Freedom KL25Z LEDs
 #define RED_LED_POS (18)		// on port B
@@ -12,3 +12,4 @@
 #define BLUE_LED_POS (1)		// on port D
 
 #endif
+*/
--- Source_code/gpio_defs.h
+++ Source_code/gpio_defs.h
@@ -3,4 +3,13 @@
 
 #define MASK(x) (1UL << (x))
 
+// Freedom KL25Z LEDs
+#define RED_LED_POS (18)		// on port B
+#define GREEN_LED_POS (19)		// on port B
+#define BLUE_LED_POS (1)		// on port D
+
+#include <stdint.h>
+
+extern void Delay(uint32_t dlyTicks);
+
 #endif
